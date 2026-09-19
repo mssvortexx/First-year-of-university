5d0c71a757efd8a23a79aee7ab428300988f09aa
Светка <mssvortex@icloud.com>
2026-09-19 18:20:18 +0300

    .

--- lab61.cpp
+++ lab61.cpp
@@ -10,10 +10,16 @@ int main()
     setlocale(LC_ALL, ".UTF-8");
 
     float x_first = get_number("х начальный элемент");
+    float x = x_first;
     float x_last = get_number("х конечный элемент");
     float x_delta = get_number("шаг х");
 
-
+    do
+    {
+        cout << "F(" << x << ") = " << 2.5 * pow(x, 3) + (3.2 / (x - 2)) + pow(sin(x), 3) << endl;
+        x += x_delta;
+    } while (x <= x_last);
+    
     return 0;
 }
 
